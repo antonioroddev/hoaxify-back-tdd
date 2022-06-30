f058c1d0bf2a5bff44af085e4d4554495a78de4e
Antonio Rodrigues <antonioroddev@protonmail.com>
2022-06-29 23:02:58 -0300

    test: add test for saving user in database

--- src/test/java/com/hoaxfyu/hoaxify/UserControllerTest.java
+++ src/test/java/com/hoaxfyu/hoaxify/UserControllerTest.java
@@ -1,4 +1,8 @@
-import User.User;
+package com.hoaxfyu.hoaxify;
+
+import com.hoaxfyu.hoaxify.User.User;
+import com.hoaxfyu.hoaxify.User.UserRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,18 +22,38 @@ public class UserControllerTest {
 
     @Autowired
     TestRestTemplate testRestTemplate;
+    @Autowired
+    UserRepository userRepository;
+    private String PATH = "/api/1.0/users";
+
 
     // Name Scheme = methodName_condition_expectedBehaviour
     @Test
     public void postUser_whenUserIsValid_receiveOk(){
+        User user = createValidUser();
+        ResponseEntity<Object> response = testRestTemplate.postForEntity(PATH, user, Object.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    public void postUser_whenUserIsValid_userSavedToDataBase(){
+        User user = createValidUser();
+        testRestTemplate.postForEntity(PATH, user, Object.class);
+        assertThat(userRepository.count()).isEqualTo(1);
+
+    }
+
+    private User createValidUser(){
         User user = new User();
         user.setUsername("test-Username");
         user.setDisplayName("test-displayName");
         user.setPassword("test-password");
+        return user;
+    }
 
-        ResponseEntity<Object> response = testRestTemplate.postForEntity("/api/1.0/users", user, Object.class);
-
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    @Before
+    public void cleanup(){
+        userRepository.deleteAll();
     }
 
 }
