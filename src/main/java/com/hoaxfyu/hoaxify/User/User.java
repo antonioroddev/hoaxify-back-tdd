727ddcf7268602ca2915b957e30e655392c7ef25
Antonio Rodrigues <antonioroddev@protonmail.com>
2022-06-29 23:03:55 -0300

    feat: implements create user in user controller

--- src/main/java/com/hoaxfyu/hoaxify/User/User.java
+++ src/main/java/com/hoaxfyu/hoaxify/User/User.java
@@ -1,10 +1,18 @@
-package User;
+package com.hoaxfyu.hoaxify.User;
 
 import lombok.*;
 
+import javax.persistence.*;
+import java.io.Serializable;
+
 @Data
+@Entity
+@Table(name="users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String username;
     private String displayName;
     private String password;
