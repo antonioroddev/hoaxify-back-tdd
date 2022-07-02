47a4c32555c13b6dec4a3af6eb892d0f36769e29
Antonio Rodrigues <antonioroddev@protonmail.com>
2022-07-02 18:50:45 -0300

    feat: implements user password encoding

--- src/main/java/com/hoaxfyu/hoaxify/User/UserService.java
+++ src/main/java/com/hoaxfyu/hoaxify/User/UserService.java
@@ -1,18 +1,23 @@
 package com.hoaxfyu.hoaxify.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
+    private BCryptPasswordEncoder passwordEncoder;
     private UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
+        this.passwordEncoder = new BCryptPasswordEncoder();
         this.userRepository = userRepository;
     }
 
     public User save(User user){
-      return userRepository.save(user);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
     }
 }
